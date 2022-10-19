f295fa5f77ed9dc4e4d0594f54113738c93f9e06
bikti <biktimirov_2003@bk.ru>
2022-10-20 00:31:59 +0500

    add auth with firebase

--- app/src/main/java/com/example/bulletinboard/MainActivity.kt
+++ app/src/main/java/com/example/bulletinboard/MainActivity.kt
@@ -7,14 +7,17 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
 import com.example.bulletinboard.databinding.ActivityMainBinding
+import com.example.bulletinboard.dialogs.DialogHelper
 import com.google.android.material.navigation.NavigationView.OnNavigationItemSelectedListener
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.main_content.*
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
 
     private var _binding: ActivityMainBinding? = null
     private val binding get() = _binding!!
+
+    private val dialogHelper = DialogHelper(this)
+    val mAuth = FirebaseAuth.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,16 +28,15 @@ class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
 
     private fun init() {
 
-
         val toggle = ActionBarDrawerToggle(
             this,
-            drawerLayout,
-            toolbar,
+            binding.drawerLayout,
+            binding.mainContent.toolbar,
             R.string.open,
             R.string.close
         )
 
-        drawerLayout.addDrawerListener(toggle)
+        binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         binding.navView.setNavigationItemSelectedListener(this)
     }
@@ -70,6 +72,7 @@ class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
             }
 
             R.id.id_sign_in -> {
+                dialogHelper.createSignDialog(DialogHelper.SIGN_IN)
                 Toast.makeText(this, item.title, Toast.LENGTH_LONG).show()
             }
 
@@ -78,6 +81,7 @@ class MainActivity : AppCompatActivity(), OnNavigationItemSelectedListener {
             }
 
             R.id.id_sign_up -> {
+                dialogHelper.createSignDialog(DialogHelper.SIGN_UP)
                 Toast.makeText(this, item.title, Toast.LENGTH_LONG).show()
             }
         }
